613425c5272fbe76339410bab344777ae0e43f98
aaronparker <aaron@stealthpuppy.com>
2016-04-03 22:35:19 +1000

    Update Working-Set-HypervDefaults.ps1

--- HyperV/Working-Set-HypervDefaults.ps1
+++ HyperV/Working-Set-HypervDefaults.ps1
@@ -1,17 +1,33 @@
-# Authenticate to the remote host
-$cred = Get-Credential -UserName .\Administrator -Message "Please enter the password."
-$cimsession = New-CimSession -Credential $cred -ComputerName nuc1
+# Authenticate to the remote host (CredSSP already configured for workgroup machines)
+$cred = Get-Credential -UserName Administrator -Message "Please enter the password."
+$nuc1 = New-CimSession -Credential $cred -ComputerName nuc1
 
 # Get configuration from remote host
-$VmHost = Get-VMHost -CimSession $cimsession | Select * | Format-List
-
+$VmHost = Get-VMHost -CimSession $nuc1 | Select * | Format-List
 $VmHost.VirtualHardDiskPath
 $VmHost.VirtualMachinePath
 
+# Get configuration from a VM
+$VM = Get-VM -CimSession $nuc1 -Name DC1 | Select Path, ConfigurationLocation, CheckpointFileLocation, SmartPagingFilePath, SnapshotFileLocation
+$VM.CheckpointFileLocation
+$VM.ConfigurationLocation
+$VM.SmartPagingFilePath
+$VM.SnapshotFileLocation
+$VM.Path
+
+# View path properties on all VMs
+Get-Vm -CimSession $nuc1 | Select Name, Path, ConfigurationLocation, CheckpointFileLocation, SmartPagingFilePath, SnapshotFileLocation
+
 
 # Set default folders
-
 Machines
 Virtual Hard Disks
 Checkpoints
 Smart Paging File
+
+# Set all DVD drives on a VM to 'None'
+Get-VMDvdDrive -CimSession $nuc1 -VMName DC1 | ForEach { Set-VMDvdDrive -VMDvdDrive $_ -Path $Null }
+
+# Move all VMs where XML configuration is stored in specific location to new location
+Get-VM -CimSession $nuc1 | Where-Object { $_.ConfigurationLocation -eq "D:\Hyper-V\Machines" } | Move-VMStorage -VirtualMachinePath D:\Hyper-V -Verbose
+
--- MDT/Working-MDT-Commands.ps1
+++ MDT/Working-MDT-Commands.ps1
@@ -7,3 +7,6 @@ Update-MDTDeploymentShare -path "DS001:" -Force -Verbose
 Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
 New-PSDrive -Name "DS002" -PSProvider MDTProvider -Root "\\MCFLY\Deployment\Reference"
 import-mdttasksequence -path "DS002:\Task Sequences\Vanilla OS" -Name "Windows Server" -Template "Server.xml" -Comments "" -ID "001" -Version "1.0" -OperatingSystemPath "DS002:\Operating Systems\Volume License\Windows Server 2012\Windows Server 2012 SERVERSTANDARDCORE in Windows Server 2012 VL install.wim" -FullName "stealthpuppy" -OrgName "stealthpuppy" -HomePage "about:blank" -Verbose
+
+# Remove a folder
+remove-item -path "DS002:\Operating Systems\Volume License\Windows Server 2012" -force -verbose -recurse
