e72fc7e40eb7334128976fab8ad53d56f74383d8
aaronparker <aaron@stealthpuppy.com>
2016-04-05 22:41:17 +1000

    AD and network scripts

--- AD-DNS/Promote-DomainController.ps1
+++ AD-DNS/Promote-DomainController.ps1
@@ -18,3 +18,26 @@ Install-ADDSDomainController `
 -Force:$true
 
 Get-ADDomainController -Discover -Service PrimaryDC
+
+
+# ---------
+
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Install-ADDSDomainController `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$false `
+-Credential (Get-Credential) `
+-CriticalReplicationOnly:$false `
+-DatabasePath "C:\windows\NTDS" `
+-DomainName "home.stealthpuppy.com" `
+-InstallDns:$true `
+-LogPath "C:\windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SiteName "Home" `
+-SysvolPath "C:\windows\SYSVOL" `
+-Force:$true
+
--- HyperV/Delete-VM.ps1
+++ HyperV/Delete-VM.ps1
@@ -1,6 +1,14 @@
-﻿$vmHostName = Read-Host -Prompt "Enter host name"
+﻿$Username = "Administrator"
+$Password = "Passw0rd"
+$SPassword = convertto-securestring -String $Password -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $Username, $SPassword
+# $cred = Get-Credential -UserName Administrator -Message "Please enter the password."
+$nuc1 = New-CimSession -Credential $cred -ComputerName nuc1
+
+$vmHostName = Read-Host -Prompt "Enter host name"
 $vmName = Read-Host -Prompt "Enter VM name"
-$vm = Get-VM -ComputerName $vmHostName -Name $vmName
+$vm = Get-VM -CimSession $nuc1 -Name $vmName
+# $vm = Get-VM -ComputerName $vmHostName -Name $vmName
 $VHDs = $vm | Get-VMHardDiskDrive
 Invoke-Command -ComputerName $vmHostName -ScriptBlock { param($VHDs) ForEach ( $vhd in $VHDs) { Remove-Item -Path $vhd.Path -Force -Confirm:$False -Verbose } } -Args $VHDs
 $vm | Remove-VM -Force -Verbose
